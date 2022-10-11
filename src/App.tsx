404b557f5ad873f874c2a7f45f4237b7192fffd6
okaken2022 <okaken.address@gmail.com>
2022-10-11 15:47:13 +0900

    handleSubmit メソッド

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,51 @@
 import "./styles.css";
-// react-hook-formから、useFormとSubmitHandlerをimport
-// SubmitHandlerは、submitイベントに関する関数の型宣言に使う
-import { useForm, SubmitHandler } from "react-hook-form";
+import { useForm, SubmitHandler, SubmitErrorHandler } from "react-hook-form";
 
-// フォームの入力値についての型定義。useFormフックを書く時に使う。
 type FormInput = {
-  name: string;
-  age: number;
-  
+  lastName: string;
+  firstName: string;
 };
 
 export default function App() {
-  // useFormフックを使い、registerとhandleSubmitを取得する。
-  // registerは、フォームのフィールドを登録することで、バリデーションを機能させる。
-  // handleSubmitは、submitイベントの制御に関わる。
-  const { register, handleSubmit, formState: { errors } } = useForm<FormInput>(
-    // 初期値として、名前の入力欄に「山田太郎」・年齢の入力欄に「25」を与える
-    { 
-      // mode: 'onBlur', //フィールドのフォーカス状態が外れた時の bulr イベントに紐づいて、バリデーションを実行する。 
-      defaultValues: { name: "山田太郎", age: 25 } });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<FormInput>();
 
-    
-  // submitイベントが発生して、かつバリデーションが成功した場合に実行する関数。
-  // サンプルコードなので、入力値（data）をコンソール出力するだけの処理を用意。
-  const onSubmit: SubmitHandler<FormInput> = (data) => console.log(data);
+  // バリデーションが成功した場合に実行。dataには、バリデーションが成功したフィールド情報が含まれている。
+  const success: SubmitHandler<FormInput> = (data) => console.log(data);
+
+  // バリデーションが失敗した場合に実行。errosには、バリデーションが失敗したフィールド情報が含まれている。
+  const error: SubmitErrorHandler<FormInput> = (erros) => console.log(erros);
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={handleSubmit(success, error)}>
+      <p>
+      handleSubmit メソッドは、バリデーションの成否によって、引数に渡した関数（コールバック関数）の実行を振り分けられます。<br />
+handleSubmit メソッド の第 1 引数のコールバック関数は、フォームのフィールドに対する全てのバリデーションが成功した合にみ、実行されます。一方、第 2 引数にコールバック関数は、フィールドのバリデーションがどれか 1 つでも失敗した場合に、実行されます。<br />
+
+TypeScript で書く場合は、コールバック関数の props 型の宣言用に、 SubmitHandler と SubmitErrorHandler が用意されています。
+
+      </p>
       <label>
-        名前
-        <input {...register("name", { required: true,
-        disabled: true
-        })} />
-      </label>
-      {/* 未入力の場合は、バリデーションが失敗してエラーになる */}
-      {/* erros.nameが値が入る（undefind）ので、右辺のpタグが評価される */}
-      {errors.name && <p className="error">名前欄の入力は必須です</p>}
-      <label>
-      
-        年齢
+        姓
         <input
-          type="number"
-          {...register("age", {
-            // バリデーションごとにエラーメッセージを設定する場合は、オブジェクト形式でvalueとmessageを設定する
-            required: { value: true, message: "年齢欄の入力は必須です" },
-            min: { value: 0, message: "入力できる最小値は0です" },
-            max: { value: 150, message: "入力できる最大値は150です" }
+          {...register("lastName", {
+            required: { value: true, message: "名の入力は必須です" }
           })}
         />
-        歳
       </label>
-      {/* errors.age.messageを参照すると、バリデーションが失敗した項目のメッセージが取得できる */}
-      {errors.age && <p className="error">{errors.age.message}</p>}
+      {errors.lastName && <p className="error">{errors.lastName.message}</p>}
+      <label>
+        名
+        <input
+          {...register("firstName", {
+            required: { value: true, message: "名の入力は必須です" }
+          })}
+        />
+      </label>
+      {errors.firstName && <p className="error">{errors.firstName.message}</p>}
       <button>送信する</button>
     </form>
   );
